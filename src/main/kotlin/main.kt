afde65e3719c00a37e45368520451e01121c8590
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-06-21 21:28:42 +0300

    rename extension files

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,24 +6,23 @@ fun main() {
     val readFile = ReadFile()
     val file = readFile.readFile(FilePath.GOOGLE_PLAY_JSON)
 
-    val dataSource: List<App> = JSONParser().getAllApps(file)
+    val apps: List<App> = JSONParser().getAllApps(file)
 
     with(Analyzer()) {
         println("Number of Apps Developed by Google: " +
-            {countAppsDevelopedBySpecificCompany(dataSource, "Google")})
+            {countAppsDevelopedBySpecificCompany(apps, "Google")})
         println("\nPercentage of medical apps: " +
-                findPercentageOfSpecificApp(dataSource, "Medical"))
+                findPercentageOfSpecificApp(apps, "Medical"))
         println("\nOldest apps: " +
-                getOldestApp(dataSource))
+                getOldestApp(apps))
         println("\nPercentage of apps running on Android 9.0: " +
-                calculatePercentageOfAppsRunningOnSpecificVersion(dataSource, 9.0))
+                calculatePercentageOfAppsRunningOnSpecificVersion(apps, 9.0))
         println("\nLargest app developed by Google: " +
-                getLargestAppByCompany(dataSource, "Google"))
+                getLargestAppByCompany(apps, "Google"))
         println("\nLargest five apps: " +
-                getLargestApps(dataSource, 5))
+                getLargestApps(apps, 5))
         println("\nTop 10 installed apps: " +
-                getTopInstalledApps(dataSource, 5))
+                getTopInstalledApps(apps, 5))
     }
 
 }
-
